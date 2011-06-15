4d8e0a76ead93119b3173020ee1281135d165490
Mavnn <michael@mavnn.co.uk>
2011-06-15 13:17:54 +0100

    Wrapper function

--- test.go
+++ test.go
@@ -11,19 +11,28 @@ var addr = flag.String("addr",  ":9999", "http service address")
 func main() {
 	flag.Parse()
 	fmt.Println("Hello, world")
-	http.Handle("/", http.HandlerFunc(testpage))
+	http.Handle("/", handlerWrapper(http.HandlerFunc(testpage)))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		fmt.Println("Oh noes.")
 	}
 }
 
-func testpage(w http.ResponseWriter, req *http.Request) {
-  req.ParseForm()
-	if value, ok := req.Form["callback"]; ok {
-		fmt.Fprintf(w, "%v(", value)
+func handlerWrapper(baseHandler http.HandlerFunc) http.HandlerFunc {
+	f := func(w http.ResponseWriter, req *http.Request) {
+		req.ParseForm()
+		callback, ok := req.Form["callback"]
+		if ok {
+			fmt.Fprintf(w, "%v(", callback)
+		}
+		baseHandler(w, req)
+		if ok {
+			fmt.Fprintf(w, ");")
+		}
 	}
+}
+
+func testpage(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, `{ some : "json" }`)
-	fmt.Fprintf(w, ");")
 }
 
